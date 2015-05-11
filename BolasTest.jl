0231cd21e4f9af3654243d04c11044b11c435f20
Antonio Rosado Gonzalez <tony_ixidor@hotmail.com>
2015-05-11 11:50:35 -0500

    tests de funciones elementales

--- BolasTest.jl
+++ BolasTest.jl
@@ -41,7 +41,45 @@ facts("Prueba básicas de aritmética:") do
     
     @fact 1*A => A
     @fact 2*B => B*2
-    @fact 0*A = Bola(0)
+    @fact 0*A => Bola(0)
+
+    
+end
+
+facts("Pruebas de funciones elementales") do
+    
+        
+    A=Bola(pi,pi/2)
+    B=Bola(1/sqrt(2),1)
+    C=Bola(1,1)
+    D=Bola(sqrt(2),1)
+    E=Bola(0,1)
+    
+    @fact sin(A) => Bola(0,(pi/2)*1)
+    @fact cos(A) => Bola(-1,(pi/2)*1)
+    @fact tan(A) => Bola(0,(pi/2)*1)
+    @fact cot(A) => Bola(Inf,(pi/2)*0)
+    @fact sec(A) => Bola(-1,(pi/2)*0)
+    @fact csc(A) => Bola(Inf,(pi/2)*Inf)
+    
+    @fact asin(B) => Bola(pi/4,sqrt(2))
+    @fact acos(B) => Bola(pi/4,sqrt(2))
+    @fact atan(C) => Bola(pi/4,.5)
+    @fact acot(C) => Bola(pi/4,.5)
+    @fact asec(D) => Bola(pi/4,1/sqrt(2))
+    @fact acsc(D) => Bola(pi/4,1/sqrt(2))
+    
+    @fact sinh(E) => Bola(0,1)
+    @fact cosh(E) => Bola(1,0)
+    @fact tanh(E) => Bola(0,1)
+    @fact coth(E) => Bola(Inf,Inf)
+    @fact sech(E) => Bola(1,0)
+    @fact csch(E) => Bola(Inf,Inf)
+    
+    @fact exp(E) => Bola(1,1)
+    @fact log(C) => Bola(0,1)
+    
+    
 
     
 end
